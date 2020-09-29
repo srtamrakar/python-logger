8dd5236fa375ef0ebd8aafb5bc7c1e9c9f24b733
Samyak Tamrakar <samyak.r.tamrakar@gmail.com>
2020-09-29 14:44:42 +0200

    updated dependency name

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-module_version = "0.4.0"
+module_version = "0.4.1"
 
 dependencies_list = [
     "multiprocessing_logging==0.3.1",
     "pyfiglet==0.7",
-    "python-json-logger=2.0.0",
+    "python_json_logger==2.0.0",
 ]
 
 setup(
